73934f139220ac5deb3c8ce041d1d73398553387
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-11 00:36:17 +0500

    chore: add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import s from './App.module.css'
+import { Frame } from './components/Frame/Frame'
+import { restoreState, saveState } from './utils/local-storage'
 
-function App() {
+
+export const App = () => {
+  const frames: FramesT[] = [
+    { view: 'settings' },
+    { view: 'counter' },
+  ]
+  const [state, setState] = useState<StateT>({
+      values: restoreState('values', {
+        counterValue: 0,
+        minValue: 0,
+        maxValue: 0,
+        startValue: 0,
+        resetValue: 0,
+      }),
+      controls: {
+        incValue() {
+          setState((prevValues) => ({
+            ...prevValues,
+            values: { ...prevValues.values, counterValue: prevValues.values.counterValue + 1 },
+          }))
+        },
+        decValue() {
+          setState((prevValues) => ({
+            ...prevValues,
+            values: { ...prevValues.values, counterValue: prevValues.values.counterValue - 1 },
+          }))
+        },
+        resValue() {
+          setState((prevValues) => ({
+            ...prevValues,
+            values: { ...prevValues.values, counterValue: prevValues.values.startValue },
+          }))
+        },
+        setMinValue(value: number) {
+          setState((prevValues) => ({ ...prevValues, values: { ...prevValues.values, minValue: value } }))
+        },
+        setMaxValue(value: number) {
+          setState((prevValues) => ({ ...prevValues, values: { ...prevValues.values, maxValue: value } }))
+        },
+        setStartValue(value: number) {
+          setState((prevValues) => ({ ...prevValues, values: { ...prevValues.values, startValue: value } }))
+        },
+        setCounterValue(value: number) {
+          setState((prevValues) => ({ ...prevValues, values: { ...prevValues.values, counterValue: value } }))
+        },
+      },
+    },
+  )
+  const [isActiveSetBtn, setIsActiveSetBtn] = useState<boolean>(true)
+
+  const [inputMinValue, setInputMinValue] = useState<number>(state.values.minValue)
+  const [inputMaxValue, setInputMaxValue] = useState<number>(state.values.maxValue)
+  const [inputStartValue, setInputStartValue] = useState<number>(state.values.startValue)
+  const [inputCounterValue, setInputCounterValue] = useState<number>(state.values.counterValue)
+
+  useEffect(() => {
+    saveState('values', state.values)
+  }, [state.values])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.App}>
+      {
+        frames.map(f =>
+          <Frame key={f.view}
+                 view={f.view}
+                 state={state}
+                 isActiveSetBtn={isActiveSetBtn}
+                 setIsActiveSetBtn={setIsActiveSetBtn}
+                 inputMinValue={inputMinValue}
+                 inputMaxValue={inputMaxValue}
+                 inputStartValue={inputStartValue}
+                 inputCounterValue={inputCounterValue}
+                 setInputMinValue={setInputMinValue}
+                 setInputMaxValue={setInputMaxValue}
+                 setInputStartValue={setInputStartValue}
+                 setInputCounterValue={setInputCounterValue}
+          />,
+        )
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+// Types
+export type ViewsT = 'settings' | 'counter'
+export type FramesT = { view: ViewsT }
+export type StateT = {
+  values: ValuesT
+  controls: ControlsT
+}
+export type ValuesT = {
+  counterValue: number,
+  startValue: number,
+  minValue: number,
+  maxValue: number,
+  resetValue: number,
+}
+export type ControlsT = {
+  incValue: () => void
+  decValue: () => void
+  resValue: () => void
+  setCounterValue: (value: number) => void
+  setMinValue: (value: number) => void
+  setMaxValue: (value: number) => void
+  setStartValue: (value: number) => void
+}
